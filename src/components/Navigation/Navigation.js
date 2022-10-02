71fc1a88f7c98ebff36bf4f70df5c59703bb71da
sionyeom <duatldhs5555@gmail.com>
2022-10-02 09:48:03 +0900

    feat: delete unnecessary files

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,7 +1,5 @@
 import React from "react";
 import "./navigation.scss";
-import { ReactComponent as JordanLogo } from "../../assets/img/navigation/air-jordan-logo.svg";
-import { ReactComponent as ConverseLogo } from "../../assets/img/navigation/converse-logo.svg";
 
 import PreHeader from "./PreHeader/PreHeader";
 import Header from "./Header/Header";
