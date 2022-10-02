4f7a83a2517e7b2d8a822662092cbdb9b42d1bce
sionyeom <duatldhs5555@gmail.com>
2022-10-02 09:48:32 +0900

    feat: set logo & sort list

--- src/components/Navigation/Header/Header.js
+++ src/components/Navigation/Header/Header.js
@@ -1,11 +1,22 @@
 import React from "react";
 import "./header.scss";
+import { ReactComponent as NikeSwooshLogo } from "../../../assets/img/navigation/nike-logo.svg";
 
 const Header = () => {
   return (
     <div className="header">
-      <div className="logo_nike"></div>
-      <div className="menues"></div>
+      <div className="logo_nike">
+        <NikeSwooshLogo width="80px" height="60px" />
+      </div>
+      <div className="menues_container">
+        <ul className="menues">
+          <li>New Releases</li>
+          <li>Men</li>
+          <li>Women</li>
+          <li>Kids</li>
+          <li>Sale</li>
+        </ul>
+      </div>
       <div className="searchBar_container"></div>
       <div className="btn_container"></div>
     </div>
