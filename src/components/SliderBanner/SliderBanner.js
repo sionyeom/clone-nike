ab2fa4991c7a0d595ea846f8d5564017f67062c6
sionyeom <duatldhs5555@gmail.com>
2022-10-04 17:44:35 +0900

    faet: fix slider scss

--- src/components/SliderBanner/SliderBanner.js
+++ src/components/SliderBanner/SliderBanner.js
@@ -20,9 +20,11 @@ const SliderBanner = () => {
       </div>
       <Slider {...settings}>
         <div className="sliderbanner_slider_content">
-          고객님의 나이키 정보가 이전됩니다. <a>동의하기</a>
+          고객님의 나이키 멤버 정보가 이전됩니다. <a className="link_a">동의하기</a>
         </div>
-        <div className="sliderbanner_slider_content">2</div>
+        <div className="sliderbanner_slider_content">
+          <a className="link_a">개인정보 처리방침 개편 안내 </a>/<a className="link_a">이용약관 개편 안내</a>
+        </div>
       </Slider>
     </div>
   );
