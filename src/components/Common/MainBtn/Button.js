3ac5ff2ab8e45e1865c413e7e2f6f03d8dd62fd6
sionyeom <duatldhs5555@gmail.com>
2022-10-05 17:49:04 +0900

    feat: add whitetheme btn

--- src/components/Common/MainBtn/Button.js
+++ src/components/Common/MainBtn/Button.js
@@ -1,12 +1,18 @@
-import React from "react";
 import "./button.scss";
+import React, { useEffect, useState } from "react";
 
 const Button = props => {
-  const { text } = props;
-  console.log(text);
+  const { themetype, text } = props;
+  const [classname, setClassname] = useState("main_btn");
+  // 화이트 테마 버튼
+  useEffect(() => {
+    if (themetype) {
+      setClassname("main_btn white");
+    }
+  });
 
   return (
-    <a className="main_btn">
+    <a className={classname}>
       {text}
     </a>
   );
